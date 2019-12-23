3588fd4c709811edd7bfd04f27cafa9efc2165cd
Nathaniel Schaaf <schaafna@gmail.com>
2019-12-23 13:01:54 +0100

    Add basic snake game rules and board drawing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='skillsusa',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Nathaniel Schaaf',
+    author_email='',
+    description=''
+)
